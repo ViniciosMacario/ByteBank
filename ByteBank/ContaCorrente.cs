4fc529f8dc0541dceb5b087cf3531726598b5e81
ViniciosMacario <JackWyus_01@outlook.com>
2023-04-17 17:40:57 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+        //Criando um método para adicionar novos valores no saldo final.
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+    }
+
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using ByteBank;
+
+//precisamos definir o tipo do objeto no caso é conta corrente
+//new, que é o operador que nos permite criar um novo objeto
+ContaCorrente contaDoUsuario = new ContaCorrente();
+contaDoUsuario.titular = "Vinicios";
+contaDoUsuario.numero_agencia = 10;
+contaDoUsuario.saldo = 2000;
+contaDoUsuario.conta = "1010x";
+
+Console.WriteLine($"Saldo da Conta do {contaDoUsuario.titular} = R$ {contaDoUsuario.saldo}");
+contaDoUsuario.Depositar(150);
+Console.WriteLine(contaDoUsuario.saldo);
