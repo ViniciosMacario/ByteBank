794193b00048935846ac2a3763cc6485e55b6a5f
ViniciosMacario <JackWyus_01@outlook.com>
2023-04-20 17:58:08 -0300

    New update

--- ByteBank/Programa.cs
+++ ByteBank/Programa.cs
@@ -1,15 +1,35 @@
 ﻿using ByteBank.Contas;
-using ByteBank.Titular;
 
+namespace SistemaBancario
+{
+    class Programa
+    {
+        static void Main()
+        {
+            //Estamos criando nossas variáveis para armarzenar o valor digitado pelo usário
+            string Nome;
+            int Idade;
+            string Cpf;
 
-//O método construtor é invocado quando damos um new no objeto. No "Program.cs" ao dar um new ContaCorrente() estamos invocando o método construtor que vai construir na memória dos computadores um objeto do tipo conta corrente.
-ContaCorrente conta5 = new ContaCorrente(283, "1234-X");
-Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.WriteLine(" Bem Vindo ao ByteBank");
+            Console.WriteLine(" Vamos criar uma conta, por gentileza, os seguintes dados são necessários:");
 
-ContaCorrente conta6 = new ContaCorrente(284, "9874-Z");
-Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.Write(" - Name : ");
+            Nome = Console.ReadLine();
 
-ContaCorrente conta7 = new ContaCorrente(285, "1111-Z");
-Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.Write(" - Age  : ");
+            Idade = int.Parse(Console.ReadLine());
 
-// Então, ContaCorrente.TotalDeContasCriadas é uma propriedade estática que carrega no momento que executa a aplicação. E os objetos têm acesso às propriedades que fazem parte da classe da qual eles se originam, por isso conseguimos fazer o incremento da propriedade no construtor da classe.
+            Console.Write(" - CPF  : ");
+            Cpf = Console.ReadLine();
+
+            new CreateAccount(
+                Nome: Nome,
+                Idade: Idade,
+                Cpf: Cpf
+            );
+
+        }
+
+    }
+}
